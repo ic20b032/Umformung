ee568a1754ac1f0b559c8cd29a6051361bf9cd7d
ic20b032 <ic20b032@technikum-wien.at>
2022-12-06 20:31:33 +0100

    #2 add Test für Calculation

--- Umformung/src/test/java/at/fhtechnikum/Umformung/UmformungApplicationTests.java
+++ Umformung/src/test/java/at/fhtechnikum/Umformung/UmformungApplicationTests.java
@@ -2,12 +2,35 @@ package at.fhtechnikum.Umformung;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class UmformungApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testInchToCentimeter() {
+
+		// Given
+		UmformungController controller = new UmformungController();
+
+		// When
+		double result = controller.InchToCentimeter(3);
+
+		// Then
+		assertEquals(result, 60);
+
+	}
+	void testYardToMeter() {
+
+		// Given
+		UmformungController controller = new UmformungController();
+
+		// When
+		double result = controller.YardToMeter(3);
+
+		// Then
+		assertEquals(result, 60);
+
 	}
 
 }
