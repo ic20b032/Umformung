425728319dc993e6e730b04ad568a2d68a868617
ic20b032 <ic20b032@technikum-wien.at>
2022-12-06 19:52:26 +0100

    commit UmformungController

---
+++ Umformung/src/main/java/at/fhtechnikum/Umformung/UmformungApplication.java
@@ -0,0 +1,13 @@
+package at.fhtechnikum.Umformung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmformungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmformungApplication.class, args);
+	}
+
+}
